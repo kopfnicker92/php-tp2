4cc2c2cf7629e97c9faa97804aa05715527f03f7
kopfnicker92 <florvandenburg@gmail.com>
2015-09-27 22:38:37 +0200

    update tp2

--- opengraph.inc.php
+++ opengraph.inc.php
@@ -1,3 +1,6 @@
-<meta property="og:title" content="Le formulaire pour devenir bleu" />
-<meta property="og:description" content="Formulaire pour accéder au cercle de l'ESIAJ" />
-<meta property="og:url" content="florianvandenburg.be/projets/serverside2">
+<meta property="og:title" content="Le formulaire pour devenir bleu">
+<meta property="og:site_name" content="Formulaire bleu">
+<meta property="og:url" content="http://florianvandenburg.be/projets/serverside2">
+<meta property="og:description" content="Formulaire pour accéder au cercle de l'ESIAJ">
+<meta property="fb:app_id" content="">
+<meta property="og:type" content="website">
